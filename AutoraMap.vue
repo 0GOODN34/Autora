187ae2c89df1649d660beaaf4dc19e3e7a783e3f
0GOODN34 <46929971+0GOODN34@users.noreply.github.com>
2019-04-30 03:07:24 +0100

    Add files via upload

---
+++ AutoraMap.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="rowMap">
+
+    <l-map :zoom="zoom" :center="center">
+    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+    <l-marker :key="index" v-for="(result, index) in results" :latlng="latLng(result.lat,result.lng)">
+    <l-icon :icon-size="iconSize":icon-url="icon"
+    </l-marker>
+  </l-map>
+
+
+
+  </div>
+</template>
+
+<script>
+import {LMap, LTileLayer, LMarker, LIcon} from 'vue2-leaflet'
+import food from '../assets/Food.png'
+  export default {
+    name: "AutoraMap",
+    props:{
+      results:Array
+    },
+    data:function(){
+      return{
+      zoom:13,
+      center: L.latLng(47.413220, -1.219482),
+      url:'https://tile.thunderforest.com/transport/{z}/{x}/{y}.png?apikey=30bc173714a74f1391503ef7ad21adb0',
+      attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      marker: L.latLng(47.413220, -1.219482)}
+    },
+    components:{
+      LMap,
+      LTileLayer,
+      LMarker,
+      LIcon
+    },
+    methods:{
+      latLng: function(lat, lng){
+        return L.latLng(lat, lng);
+      },
+
+    }
+  }
+</script>
+
+<style scoped>
+  .map{
+    height:95vh;
+  }
+</style>
